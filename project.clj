57c41146b5be7f747cfa6e9e4dc06984dd23ec39
Steve Miner <steveminer@gmail.com>
2013-05-30 20:44:40 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.velisco/halfbaked "0.1.4"
+(defproject com.velisco/halfbaked "0.1.5"
   :description "My collection of half-baked, semi-useful Clojure code."
   :url "http://github.com/miner/halfbaked"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :repl-options {:init-ns miner.halfbaked}
+  :repositories [["clojars" {:url "https://clojars.org/repo" :creds :gpg}]]
   )
 
